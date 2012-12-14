bitlbee/bitlbee-steam

commit 9e174133f428d1834a41c75b9228b5605204d7c5

steam-api: set a custom BitlBee agent/client
Initially, the agent/client needed to be one of the supported mobile
devices, if I recall correctly. In any event, upon reattempting, it
appears a custom agent/client can be used now. We will see how long
this lasts, but for now, we will sport a BitlBee agent/client.

steam/steam-api.c
@@ -467,7 +467,7 @@ void steam_api_auth(SteamAPI *api, const gchar *authcode,
 
     steam_http_req_params_set(req, 8,
         "format",          "xml",
-        "client_id",       "DE45CD61",
+        "client_id",       STEAM_API_CLIENT_ID,
         "grant_type",      "password",
         "username",        user,
         "password",        pass,

steam/steam-api.h
@@ -21,8 +21,9 @@
 #include "steam-http.h"
 
 #define STEAM_API_HOST        "api.steampowered.com"
-#define STEAM_API_AGENT       "Steam 1291812 / iPhone"
-#define STEAM_API_KEEP_ALIVE  "30" /* >= 30 defaults to 30 */
+#define STEAM_API_AGENT       "Steam / BitlBee / " PACKAGE_VERSION " / 0"
+#define STEAM_API_CLIENT_ID   "DAB33B22" /* Da Bee Bzz? */
+#define STEAM_API_KEEP_ALIVE  "30"       /* Max of 30 seconds */
 
 #define STEAM_PATH_AUTH       "/ISteamOAuth2/GetTokenWithCredentials/v0001"
 #define STEAM_PATH_FRIENDS    "/ISteamUserOAuth/GetFriendList/v0001"

commit 875b141cb6b9f2238683cc9d21fc60881e844bc6

steam-api: fixed failing GetTokenWithCredentials requests
This issue was introduced with 9e17413. A known agent/client id is
needed to actually get an API token. After a valid token is obtained,
the custom BitlBee agent can be used for all other requests.

The known agent was also switched to the Android agent this time around.

steam/steam-api.c
@@ -490,6 +490,8 @@ void steam_api_auth(SteamAPI *api, const gchar *authcode,
     req = steam_http_req_new(api->http, STEAM_API_HOST, 443,
                              STEAM_PATH_AUTH, steam_api_cb, fp);
 
+    steam_http_req_headers_set(req, 1, "User-Agent", STEAM_API_AGENT_AUTH);
+
     steam_http_req_params_set(req, 8,
         "format",          "xml",
         "client_id",       STEAM_API_CLIENT_ID,

steam/steam-api.h
@@ -21,9 +21,12 @@
 #include "steam-http.h"
 
 #define STEAM_API_HOST        "api.steampowered.com"
-#define STEAM_API_AGENT       "Steam / BitlBee / " PACKAGE_VERSION " / 0"
-#define STEAM_API_CLIENT_ID   "DAB33B22" /* Da Bee Bzz? */
-#define STEAM_API_KEEP_ALIVE  "30"       /* Max of 30 seconds */
+#define STEAM_API_AGENT       "Steam App / BitlBee / " PACKAGE_VERSION " / 0"
+#define STEAM_API_KEEP_ALIVE  "30" /* Max of 30 seconds */
+
+/* Required for GetTokenWithCredentials */
+#define STEAM_API_AGENT_AUTH  "Steam App / Android / 1.0 / 0"
+#define STEAM_API_CLIENT_ID   "DE45CD61" /* The "public" mobile client id */
 
 #define STEAM_PATH_AUTH       "/ISteamOAuth2/GetTokenWithCredentials/v0001"
 #define STEAM_PATH_FRIENDS    "/ISteamUserOAuth/GetFriendList/v0001"