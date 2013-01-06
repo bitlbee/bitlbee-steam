bitlbee/bitlbee-steam

commit 259c2bd00bc0524c66c0d7aa16caee538d1df43f

Use g_slist_prepend over g_slist_append, it is more efficient

steam/steam-api.c
@@ -161,7 +161,7 @@ static void steam_api_friends_cb(SteamFuncPair *fp, struct xt_node *xr)
         if (!steam_util_xt_node(xn, "steamid", &xe))
             continue;
 
-        fl = g_slist_append(fl, xe->text);
+        fl = g_slist_prepend(fl, xe->text);
     }
 
     err = (fl != NULL) ? STEAM_ERROR_SUCCESS : STEAM_ERROR_EMPTY_FRIENDS;
@@ -323,7 +323,7 @@ static void steam_api_poll_cb(SteamFuncPair *fp, struct xt_node *xr)
             continue;
         }
 
-        mu = g_slist_append(mu, sm);
+        mu = g_slist_prepend(mu, sm);
     }
 
     steam_list_func(fp, mu, STEAM_ERROR_SUCCESS);
@@ -374,7 +374,7 @@ static void steam_api_summaries_cb(SteamFuncPair *fp, struct xt_node *xr)
         if (steam_util_xt_node(xn, "realname", &xe))
             ss->realname = xe->text;
 
-        mu = g_slist_append(mu, ss);
+        mu = g_slist_prepend(mu, ss);
     }
 
     err = (mu != NULL) ? STEAM_ERROR_SUCCESS : STEAM_ERROR_EMPTY_SUMMARY;

steam/steam-http.c
@@ -296,6 +296,6 @@ void steam_http_req_send(SteamHttpReq *req)
     req->request = http_dorequest(req->host, req->port, ssl, sreq,
                                   steam_http_req_cb, req);
 
-    req->http->requests = g_slist_append(req->http->requests, req);
+    req->http->requests = g_slist_prepend(req->http->requests, req);
     g_free(sreq);
 }

commit c6dfb593cfce156f07c32e403866d2bf9c6eda46

steam-api: fixed poll reply messages being reversed
The issue was introduced with 259c2bd. A simple fix to the issue is to
reverse the GSList that holds the SteamMessages.

A "better" but less efficient method would be to sort the GSList by the
timestamp of each SteamMessage. This would ensure order consistency
without relying on the poll reply's default order. This will be
implemented if more related issues to message ordering arise.

This fixes messages being sent in a reversed order.

steam/steam-api.c
@@ -293,7 +293,7 @@ static void steam_api_poll_cb(SteamApiPriv *priv, struct xt_node *xr)
         mu = g_slist_prepend(mu, g_memdup(&sm, sizeof sm));
     }
 
-    priv->rdata = mu;
+    priv->rdata = g_slist_reverse(mu);
     priv->rfunc = (GDestroyNotify) steam_slist_free_full;
 }
 