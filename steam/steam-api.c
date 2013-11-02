bitlbee/bitlbee-steam

commit 8d0df30f390e1dee42e3bfde4f6dbde9eda4b9b2

steam-api: fixed retrieved messages being duplicated
The Steam Web API seems to be returning messages that have already
been returned in the previous poll reply. It is essentially a duplicate
poll reply, as the timestamps and message IDs are consistent. Poll
replies with the same last message ID should simply be ignored.

steam/steam-api.c
@@ -671,6 +671,10 @@ static void steam_api_poll_cb(SteamApiPriv *priv, json_value *json)
     guint         i;
 
     steam_json_int(json, "messagelast", &in);
+
+    if (in == priv->api->lmid)
+        return;
+
     priv->api->lmid = in;
 
     if (steam_json_str(json, "error", &str)  &&

commit 40d63fa1c402f6a92fa30cdb5b282bdee96cfe42

steam-api: fixed polling errors not being correctly handled
Commit 8d0df30 introduced a check for message IDs to ensure messages
were not doubly handled. However, with the first poll reply that errors
without containing a value for messagelast, the value will be set to 0
leading to the check failing every time after. This causes a infinite
hole from which the plugin cannot recover.

steam/steam-api.c
@@ -666,18 +666,10 @@ static void steam_api_poll_cb(SteamApiData *sata, json_value *json)
     gint64           in;
     guint            i;
 
-    steam_json_int(json, "messagelast", &in);
-
-    if (in == sata->api->lmid)
-        return;
-
-    sata->api->lmid = in;
-
     if (steam_json_str(json, "error", &str)  &&
         (g_ascii_strcasecmp(str, "Timeout") != 0) &&
         (g_ascii_strcasecmp(str, "OK")      != 0))
     {
-
         if (g_ascii_strcasecmp(str, "Not Logged On") == 0) {
             steam_api_data_relogon(sata);
             return;
@@ -687,6 +679,11 @@ static void steam_api_poll_cb(SteamApiData *sata, json_value *json)
                     "%s", str);
         return;
     }
+
+    if (!steam_json_int(json, "messagelast", &in) || (in == sata->api->lmid))
+        return;
+
+    sata->api->lmid = in;
 
     if (!steam_json_val(json, "messages", json_array, &jv))
         return;