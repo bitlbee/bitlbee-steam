e1a04219fa0d606c09dfb19de578f896af83e76c
jgeboski <jgeboski@gmail.com>
2015-01-27 20:31:43 -0500

    Check format string security at compile-time
    This enables various format string security checks by the compiler in
    attempt to avoid run-time failures.

--- steam/steam-user.h
+++ steam/steam-user.h
@@ -169,7 +169,8 @@ void steam_user_free(SteamUser *user);
 
 gint steam_user_chan_mode(const gchar *mode);
 
-void steam_user_chans_msg(SteamUser *user, const gchar *format, ...);
+void steam_user_chans_msg(SteamUser *user, const gchar *fmt, ...)
+    G_GNUC_PRINTF(2, 3);
 
 void steam_user_chans_umode(SteamUser *user, gint mode, gboolean override);
 
