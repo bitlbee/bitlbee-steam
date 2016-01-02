bitlbee/bitlbee-steam

commit 3a963839d36240643e87458a6f202d6dcdb42c91

steam-api: support SteamGuard mobile 2FA

steam/steam-api.c
@@ -188,6 +188,9 @@ static void steam_api_json_error(SteamApiReq *req, const json_value *json)
         if (steam_json_bool_chk(json, "emailauth_needed", &bool) && bool)
             return;
 
+        if (steam_json_bool_chk(json, "requires_twofactor", &bool) && bool)
+            return;
+
         if (!steam_json_str_chk(json, "message", &str))
             str = "Unknown error";
 
@@ -489,7 +492,9 @@ static void steam_api_cb_auth(SteamApiReq *req, const json_value *json)
     guint        i;
 
     if (steam_json_bool_chk(json, "success", &bool) && !bool) {
-        if (steam_json_bool_chk(json, "emailauth_needed", &bool) && bool) {
+        if (steam_json_bool_chk(json, "requires_twofactor", &bool) && bool) {
+            errc = STEAM_API_ERROR_STEAMGUARD;
+        } else if (steam_json_bool_chk(json, "emailauth_needed", &bool) && bool) {
             errc = STEAM_API_ERROR_STEAMGUARD;
             str  = steam_json_str(json, "emailsteamid");
 
@@ -505,11 +510,13 @@ static void steam_api_cb_auth(SteamApiReq *req, const json_value *json)
             errc = STEAM_API_ERROR_UNKNOWN;
         }
 
-        if (G_LIKELY(errc != STEAM_API_ERROR_UNKNOWN)) {
+        if (errc == STEAM_API_ERROR_STEAMGUARD)
+            str = "SteamGuard authentication code required";
+        else
             str = steam_json_str(json, "message");
-            g_set_error(&req->err, STEAM_API_ERROR, errc, "%s", str);
-            return;
-        }
+
+        g_set_error(&req->err, STEAM_API_ERROR, errc, "%s", str);
+        return;
     }
 
     if (!steam_json_val_chk(json, "oauth", json_string, &jv)) {
@@ -593,7 +600,7 @@ void steam_api_req_auth(SteamApiReq *req, const gchar *user, const gchar *pass,
     steam_http_req_params_set(req->req,
         STEAM_HTTP_PAIR("username",          user),
         STEAM_HTTP_PAIR("password",          pswd),
-        STEAM_HTTP_PAIR("twofactorcode",     NULL),
+        STEAM_HTTP_PAIR("twofactorcode",     authcode),
         STEAM_HTTP_PAIR("emailauth",         authcode),
         STEAM_HTTP_PAIR("emailsteamid",      req->api->esid),
         STEAM_HTTP_PAIR("captchagid",        req->api->cgid),

commit 05c7503574089237b20457ebc19aa00f238f0e71

steam-api: fixed authentication failing with SteamGuard email codes
This is a regression introduced by 3a96383.

steam/steam-api.c
@@ -499,8 +499,10 @@ static void steam_api_cb_auth(SteamApiReq *req, const json_value *json)
 
     if (steam_json_bool_chk(json, "success", &bool) && !bool) {
         if (steam_json_bool_chk(json, "requires_twofactor", &bool) && bool) {
+            req->api->autht = STEAM_API_AUTH_TYPE_MOBILE;
             errc = STEAM_API_ERROR_STEAMGUARD;
         } else if (steam_json_bool_chk(json, "emailauth_needed", &bool) && bool) {
+            req->api->autht = STEAM_API_AUTH_TYPE_EMAIL;
             errc = STEAM_API_ERROR_STEAMGUARD;
             str  = steam_json_str(json, "emailsteamid");
 
@@ -603,12 +605,29 @@ void steam_api_req_auth(SteamApiReq *req, const gchar *user, const gchar *pass,
     g_get_current_time(&tv);
     ms = g_strdup_printf("%ld", (tv.tv_usec / 1000));
 
+    switch (req->api->autht) {
+    case STEAM_API_AUTH_TYPE_EMAIL:
+        steam_http_req_params_set(req->req,
+            STEAM_HTTP_PAIR("emailauth",    authcode),
+            STEAM_HTTP_PAIR("emailsteamid", req->api->esid),
+            NULL
+        );
+        break;
+
+    case STEAM_API_AUTH_TYPE_MOBILE:
+        steam_http_req_params_set(req->req,
+            STEAM_HTTP_PAIR("twofactorcode", authcode),
+            NULL
+        );
+        break;
+
+    default:
+        break;
+    }
+
     steam_http_req_params_set(req->req,
         STEAM_HTTP_PAIR("username",          user),
         STEAM_HTTP_PAIR("password",          pswd),
-        STEAM_HTTP_PAIR("twofactorcode",     authcode),
-        STEAM_HTTP_PAIR("emailauth",         authcode),
-        STEAM_HTTP_PAIR("emailsteamid",      req->api->esid),
         STEAM_HTTP_PAIR("captchagid",        req->api->cgid),
         STEAM_HTTP_PAIR("captcha_text",      captcha),
         STEAM_HTTP_PAIR("rsatimestamp",      req->api->pktime),

steam/steam-api.h
@@ -56,6 +56,9 @@
 #define STEAM_COM_PATH_KEY           "/mobilelogin/getrsakey/"
 
 
+/** The types of authentication. **/
+typedef enum _SteamApiAuthType SteamApiAuthType;
+
 /** The #GError codes of #SteamApi. **/
 typedef enum _SteamApiError SteamApiError;
 
@@ -88,6 +91,15 @@ typedef void (*SteamApiFunc) (SteamApiReq *req, gpointer data);
  **/
 typedef void (*SteamApiParser) (SteamApiReq *req, const json_value *json);
 
+
+/**
+ * The types of authentication.
+ **/
+enum _SteamApiAuthType
+{
+    STEAM_API_AUTH_TYPE_EMAIL, /** Email **/
+    STEAM_API_AUTH_TYPE_MOBILE /** Mobile **/
+};
 
 /**
  * The #GError codes of #SteamApi.
@@ -138,6 +150,8 @@ struct _SteamApi
 
     gint64 lmid;         /** The last message identifier. **/
     gint64 time;         /** The logon timestamp (UTC). **/
+
+    SteamApiAuthType autht; /** The #SteamApiAuthType. **/
 
     gchar *cgid;         /** The captcha GID (authentication). **/
     gchar *esid;         /** The email SteamID (authentication). **/

steam/steam.c
@@ -295,6 +295,7 @@ static void steam_cb_auth(SteamApiReq *req, gpointer data)
 
     acc = sata->ic->acc;
 
+    set_setint(&acc->set, "autht",  req->api->autht);
     set_setstr(&acc->set, "cgid",   req->api->cgid);
     set_setstr(&acc->set, "esid",   req->api->esid);
     set_setstr(&acc->set, "sessid", req->api->sessid);
@@ -779,6 +780,9 @@ static void steam_init(account_t *acc)
     s = set_add(&acc->set, "captcha", NULL, steam_eval_accounton, acc);
     s->flags = SET_NULL_OK | SET_HIDDEN | SET_NOSAVE;
 
+    s = set_add(&acc->set, "autht", NULL, NULL, acc);
+    s->flags = SET_NULL_OK | SET_HIDDEN | SET_NOSAVE;
+
     s = set_add(&acc->set, "esid", NULL, NULL, acc);
     s->flags = SET_NULL_OK | SET_HIDDEN | SET_NOSAVE;
 
@@ -821,6 +825,7 @@ static void steam_login(account_t *acc)
         g_free(sata->api->esid);
         sata->api->esid = g_strdup(str);
 
+        sata->api->autht = set_getint(&acc->set, "autht");
         imcb_log(sata->ic, "Requesting authentication key");
         req = steam_api_req_new(sata->api, steam_cb_key, sata);
         steam_api_req_key(req, acc->user);